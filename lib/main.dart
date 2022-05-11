682724d85b990151cfc7f8570d91e995b800e3e3
hhimanshu <harit.himanshu@yahoo.in>
2022-05-11 13:40:01 -0700

    feat: add heart icon and interactivity to save wordpair

--- lib/main.dart
+++ lib/main.dart
@@ -67,6 +67,7 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
+  final _saved = <WordPair>{};
 
   @override
   Widget build(BuildContext context) {
@@ -79,9 +80,26 @@ class _RandomWordsState extends State<RandomWords> {
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
           }
-          return Text(
-            _suggestions[index].asPascalCase,
-            style: _biggerFont,
+          final alreadySaved = _saved.contains(_suggestions[index]);
+          return ListTile(
+              title: Text(
+                _suggestions[index].asPascalCase,
+                style: _biggerFont,
+              ),
+            trailing: Icon(
+              alreadySaved? Icons.favorite: Icons.favorite_border,
+              color: alreadySaved? Colors.red: null,
+              semanticLabel: alreadySaved? 'Remove from saved' : 'Save',
+            ),
+            onTap: () {
+                setState(() {
+                  if (alreadySaved) {
+                    _saved.remove(_suggestions[index]);
+                  } else {
+                    _saved.add(_suggestions[index]);
+                  }
+                });
+            },
           );
         });
   }
