e1822b2559ca634c6762af1b7ed2dcafe8b82b51
hhimanshu <harit.himanshu@yahoo.in>
2022-05-12 13:02:11 -0700

    feat: remove the need of MyHomePage, and use _RandomWordsState to reflect the App UI

--- lib/main.dart
+++ lib/main.dart
@@ -11,48 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Startup Names Inspiration',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.lightBlue,
-      ),
-      home: const MyHomePage(title: 'Startup Names Generator'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: const Center(
-        child: RandomWords()
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -71,27 +32,31 @@ class _RandomWordsState extends State<RandomWords> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Startup Names Generator"),
+      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (context, i) {
+            if (i.isOdd) return const Divider();
 
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          final alreadySaved = _saved.contains(_suggestions[index]);
-          return ListTile(
+            final index = i ~/ 2;
+            if (index >= _suggestions.length) {
+              _suggestions.addAll(generateWordPairs().take(10));
+            }
+            final alreadySaved = _saved.contains(_suggestions[index]);
+            return ListTile(
               title: Text(
                 _suggestions[index].asPascalCase,
                 style: _biggerFont,
               ),
-            trailing: Icon(
-              alreadySaved? Icons.favorite: Icons.favorite_border,
-              color: alreadySaved? Colors.red: null,
-              semanticLabel: alreadySaved? 'Remove from saved' : 'Save',
-            ),
-            onTap: () {
+              trailing: Icon(
+                alreadySaved? Icons.favorite: Icons.favorite_border,
+                color: alreadySaved? Colors.red: null,
+                semanticLabel: alreadySaved? 'Remove from saved' : 'Save',
+              ),
+              onTap: () {
                 setState(() {
                   if (alreadySaved) {
                     _saved.remove(_suggestions[index]);
@@ -99,8 +64,10 @@ class _RandomWordsState extends State<RandomWords> {
                     _saved.add(_suggestions[index]);
                   }
                 });
-            },
-          );
-        });
+              },
+            );
+          })
+    );
+
   }
 }
